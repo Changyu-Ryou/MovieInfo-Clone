ee64e0e671894e6aab90b9d1faa107b77bb70f60
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-04-21 03:19:46 +0900

    feat: load movies information using API

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+
+//여기서 App=component이다. 컴포넌트는 html을 return 한다.
+ReactDOM.render(<App />, document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
