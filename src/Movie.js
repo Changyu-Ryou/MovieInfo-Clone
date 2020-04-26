bcd08b409b6da696c0ff7b6f362dda108879d94d
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-04-26 23:12:11 +0900

    feat: change Info Card style

--- src/Movie.js
+++ src/Movie.js
@@ -4,21 +4,35 @@ import "./App.css";
 import "./Movie.css";
 
 function Movie({ year, title, summary, poster, genres }) {
-    return (<div className="movie">
-        <img src={poster} alt={title} title={title}/>
-        <div className="movie__data">
-            <h3 className="movie__title" >{title}</h3>
-            <h5 className="movie__year">{year}</h5>
-            <ul className="genres">
-                {genres.map((genre, index) => (
-                    
-                <li key={index} className="genres__genre">{genre}</li>
-            ))}
-            </ul>
-            <p className="movie__summary">{summary}</p>
-            
+    return (
+        <div className="movie">
+            <table>
+                <tbody>
+                <tr>
+                    <td className="posterimg">
+                        <img src={poster} alt={title} title={title} />
+                    </td>
+                    <td className="data">
+                        <div className="movie__data">
+                            <p className="movie__title" >{title}</p>
+                            <p className="movie__year">{year}</p>
+                            <ul className="genres">
+                                {genres.map((genre, index) => (
+
+                                    <li key={index} className="genres__genre">{genre}</li>
+                                ))}
+                            </ul>
+                            
+                            <p className="movie__summary">{summary}</p>
+
+                        </div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+
+
         </div>
-    </div>
     )
 }
 
